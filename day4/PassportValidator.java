756705547e6e50aedcdf3d4b73153b5ff1b11615
Fredrik Åslund <fasl@kth.se>
2020-12-09 23:27:37 +0100

    Sync to desktop

--- day4/PassportValidator.java
+++ day4/PassportValidator.java
@@ -1,12 +1,12 @@
 package day4;
 
-import day2.PasswordValidator;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Day 4 of Advent of Code 2020!
@@ -14,30 +14,21 @@ import java.util.HashMap;
  * Part one returns the number of valid passports according to the rules:
  *  cid is optional, all other fields must be present.
  *
+ * Part Two extends rules to specified valid ranges and returns number of valid passports.
+ *
  * @author Fredrik Åslund
  * @version 2020-12-08
  *
- * @field requiredPassInfo The fields a passport must have.
- * @field passports ArrayList where all the passports from input file are stored
+ * @var requiredPassInfo The fields a passport must have.
+ * @var passports ArrayList where all the passports from input file are stored
  */
 public class PassportValidator {
     private ArrayList<String> requiredPassInfo = new ArrayList<>();
     private ArrayList<HashMap> passports = new ArrayList();
 
     /**
-     * This method populates the list of passport fields.
+     * This method populates the list of required information for passports.
      */
-    /*
-    Valid ranges
-    byr: four digits, 1920-2002
-    iyr: four digits, 2010-2020
-    eyr: four digits, 2020-2030
-    hgt: either 150-193cm or 59-76in
-    hcl: # followed by six characters (0-9) or (a-f)
-    ecl: exactly one of amb blu brn gry grn hzl oth
-    pid: nine digit number, including leading zeroes
- */
-
     // Gör om till HashMap istället.
     public void populate(){
         requiredPassInfo.add("byr"); requiredPassInfo.add("iyr"); requiredPassInfo.add("eyr");
@@ -45,20 +36,31 @@ public class PassportValidator {
         requiredPassInfo.add("pid");
     }
 
-
-    public int partOne(String file){
+    /**
+     * The method that runs the program and uses helper methods to count the number of valid pasports of the input.
+     * @param file Path to the input textfile.
+     * @return the number of valid passports
+     */
+    public void partOne(String file){
         populate();
         collectInput(file);
-        int validCounter = 0;
+        int validCounter1 = 0;
+        int validCounter2 = 0;
 
         for(int i = 0; i < passports.size(); i++){
-            if(passportIsValidPartOne(passports.get(i))){
-                validCounter++;
+            if(passportIsValidPartOne(passports.get(i), 1)){
+                validCounter1++;
+                if(passportIsValidPartOne(passports.get(i), 2)){
+                    validCounter2++;
+                }
             }
-        }
-        return validCounter;
-    }
 
+        }
+        System.out.println("Total passports: "+ passports.size());
+        System.out.println("Valid passports in part one: " + validCounter1);
+        System.out.println("Valid passports in part two: " + validCounter2);
+    }
+/*
     public int partTwo(String file){
         populate();
         collectInput(file);
@@ -75,39 +77,99 @@ public class PassportValidator {
         System.out.println(results);
         return validCounter1;
     }
+ */
 
     // Gör om till validator för bägge parts på en gång. Kolla om vali innuti if-satsen!
     // Gör en for-each loop som kollar mor requiredPassInfos keyset istället för ArrayList.
     // kanske separat funktion för att kolla att värden stämmer (blir för lång annars)
-     public boolean passportIsValidPartOne(HashMap<String, String> input){
+     public boolean passportIsValidPartOne(HashMap<String, String> input, int part){
         var passport = true;
-        for(int i = 0; i< requiredPassInfo.size(); i++){
-            //String result = input.get(info);
-            if(!input.containsKey(requiredPassInfo.get(i))){
-                passport = false;
+        if(part == 1) {
+            for (int i = 0; i < requiredPassInfo.size(); i++) {
+                //String result = input.get(info);
+                if (!input.containsKey(requiredPassInfo.get(i))) {
+                    passport = false;
+                }
             }
+        }
+        if(part == 2){
+//vilken value?!?!?
+            //input.forEach((k,v) -> validValue(k,v));
+            for(int i = 0; i < requiredPassInfo.size(); i++) {
+                if(!validValue(requiredPassInfo.get(i),)){
+                    passport = false;
+                }
+            }
+
         }
         return passport;
      }
 
-    /*
- Valid ranges
- byr: four digits, 1920-2002
- iyr: four digits, 2010-2020
- eyr: four digits, 2020-2030
- hgt: either 150-193cm or 59-76in
- hcl: # followed by six characters (0-9) or (a-f)
- ecl: exactly one of amb blu brn gry grn hzl oth
- pid: nine digit number, including leading zeroes
- */
-    // Ta bort den här.
+    /**
+     * Checks if a passport value is within the allowed range.
+      * @param key The "field" to check (e.g. "byr" (birth year).
+     * @param value The value of the "field" of the passport.
+     * @return returns true if the value is valid for the key.
+     */
     public boolean validValue(String key, String value){
         var isValid = true;
-
-        if(!((key == "byr") && (value.length() == 4) && (Integer.valueOf(value) > 1919) &&(Integer.valueOf(value) < 2003))){
+        List<String> ecls = Arrays.asList("amb", "blu", "brn", "gry", "grn", "hzl", "oth");
+// Funkar
+        if(((key == "byr") && !((value.length() == 4) && (Integer.valueOf(value) > 1919) &&(Integer.valueOf(value) < 2003)))){
+            // byr: four digits, 1920-2002
             isValid = false;
         }
+// Funkar
+        if(((key == "iyr") && !((value.length() == 4) && (Integer.valueOf(value) > 2009) &&(Integer.valueOf(value) < 2021)))){
+            // iyr: four digits, 2010-2020
+            isValid = false;
+        }
+// Funkar
+        if(((key.equals("eyr")) && !((value.length() == 4) && (Integer.valueOf(value) > 2019) &&(Integer.valueOf(value) < 2031)))){
+            // eyr: four digits, 2020-2030
+        isValid = false;
+        }
+// Funkar
+        if(key == "hgt"){
+            var height = Integer.valueOf(value.substring(0,value.length()-2));
+            System.out.println(height);
+            var unit = value.substring(value.length()-2, value.length());
+            System.out.println(unit);
 
+            if((unit.equals("cm")) && (height < 150 || height > 193)){
+                isValid = false;
+            }
+            if((unit.equals("in")) && ( height < 59 || height > 76)){
+                isValid = false;
+            }
+            //hgt: either 150-193cm or 59-76in
+        }
+// Funkar!
+        if(key.equals("hcl")){
+            if(!(value.substring(0,1).equals("#")) || !(value.length() == 7)){
+                isValid = false;
+            }
+            for(int i = 1; i < value.length(); i++){
+                if(Integer.valueOf(value.charAt(i)) < 0 || 9 > Integer.valueOf(value.charAt(i))){
+                    isValid = false;
+                }
+                if(value.charAt(i) > 'f'){
+                    isValid = false;
+                }
+            }
+
+            //hcl: # followed by six characters (0-9) or (a-f)
+        }
+// Funkar!
+        if(key.equals("ecl") && (!ecls.contains(value))){
+            isValid = false;
+            //ecl: exactly one of amb blu brn gry grn hzl oth
+        }
+// Funkar!
+        if((key.equals("pid")) && (!(value.length() == 9) || !(Integer.valueOf(value) <= 999999999))){
+            isValid = false;
+            //pid: nine digit number, including leading zeroes
+        }
         return isValid;
     }
 
@@ -155,7 +217,6 @@ public class PassportValidator {
 
     public static void main(String[] args) {
         PassportValidator test = new PassportValidator();
-        System.out.println("Part one: " + test.partOne("./day4/input.txt"));
-        System.out.println("Part two: ");
+        test.partOne("./day4/input.txt");
     }
 }
--- day4/PassportValidatorTest.java
+++ day4/PassportValidatorTest.java
@@ -22,25 +22,41 @@ public class PassportValidatorTest {
     public HashMap<String, String> setUp(String input){
         HashMap<String, String> validPassport = new HashMap<>();
         HashMap<String, String> invalidPassport = new HashMap<>();
+        HashMap<String, String> invalidPassport2 = new HashMap<>();
+/*
+        Valid ranges
+        byr: four digits, 1920-2002
+        iyr: four digits, 2010-2020
+        eyr: four digits, 2020-2030
+        hgt: either 150-193cm or 59-76in
+        hcl: # followed by six characters (0-9) or (a-f)
+        ecl: exactly one of amb blu brn gry grn hzl oth
+        pid: nine digit number, including leading zeroes
+ */
+        validPassport.put("byr", "1994"); validPassport.put("iyr", "2019"); validPassport.put("eyr", "2029");
+        validPassport.put("hgt", "181cm"); validPassport.put("hcl", "#F103A0C"); validPassport.put("ecl", "blu");
+        validPassport.put("pid", "000456789");
+
+        invalidPassport2.put("byr", "2003"); invalidPassport2.put("iyr", "2009"); invalidPassport2.put("eyr", "2019");
+        invalidPassport2.put("hgt", "58in"); invalidPassport2.put("hcl", "#12345"); invalidPassport2.put("ecl", "aus");
+        invalidPassport2.put("pid", "00000678");
+
+        invalidPassport.put("byr", "2003"); invalidPassport.put("iyr", "2009"); invalidPassport.put("eyr", "2019");
+        invalidPassport.put("hgt", "58in"); invalidPassport.put("hcl", "#12345"); invalidPassport.put("ecl", "aus");
 
 
-        validPassport.put("byr", "present"); validPassport.put("iyr", "present"); validPassport.put("eyr", "present");
-        validPassport.put("hgt", "present"); validPassport.put("hcl", "present"); validPassport.put("ecl", "present");
-        validPassport.put("pid", "present");
-
-        invalidPassport.put("byr", "present"); invalidPassport.put("iyr", "present"); invalidPassport.put("eyr", "present");
-        invalidPassport.put("hgt", "present"); invalidPassport.put("hcl", "present"); invalidPassport.put("ecl", "present");
         if(input == "valid"){
         return validPassport;
         }else if(input == "invalid"){
             return invalidPassport;
-        }
+        }else if(input == "invalid2")
+            return invalidPassport2;
         return null;
     }
 
     /**
      * Assert that a valid passport returns true for part one.
-     */
+     *
     @Test
     public void assertValidPassportIsValidInPartOne(){
         // Arrange
@@ -51,10 +67,10 @@ public class PassportValidatorTest {
         test.passportIsValidPartOne(validPassport);
         assertTrue(test.passportIsValidPartOne(validPassport));
     }
-
+*/
     /**
      * Assert that an invalid passport returns false for part one.
-     */
+     *
     @Test
     public void assertInvalidPassportIsInvalidInPartOne(){
         // Arrange
@@ -64,6 +80,54 @@ public class PassportValidatorTest {
         // Act/Assert
         assertFalse(test.passportIsValidPartOne(invalidPassport));
     }
+*/
+    /*
+        Valid ranges
+        byr: four digits, 1920-2002
+        iyr: four digits, 2010-2020
+        eyr: four digits, 2020-2030
+        hgt: either 150-193cm or 59-76in
+        hcl: # followed by six characters (0-9) or (a-f)
+        ecl: exactly one of amb blu brn gry grn hzl oth
+        pid: nine digit number, including leading zeroes
+ */
+    @Test
+    public void testValidValueHGT() {
+        PassportValidator test = new PassportValidator();
+        assertFalse(test.validValue("hgt", "194cm"));
+        assertFalse(test.validValue("hgt", "77in"));
+        assertFalse(test.validValue("hgt", "149cm"));
+        assertFalse(test.validValue("hgt", "58in"));
+        assertTrue(test.validValue("hgt", "150cm"));
+        assertTrue(test.validValue("hgt", "193cm"));
+        assertTrue(test.validValue("hgt", "59in"));
+        assertTrue(test.validValue("hgt", "76in"));
+    }
+    @Test
+    public void testValidValueEYR() {
+        PassportValidator test = new PassportValidator();
+        assertFalse(test.validValue("eyr", "2019"));
+        assertTrue(test.validValue("eyr", "2020"));
+        assertTrue(test.validValue("eyr", "2030"));
+        assertFalse(test.validValue("eyr", "2031"));
+    }
 
+    @Test
+    public void testValidValueIYR() {
+        PassportValidator test = new PassportValidator();
+        assertFalse(test.validValue("iyr", "2009"));
+        assertTrue(test.validValue("iyr", "2010"));
+        assertTrue(test.validValue("iyr", "2020"));
+        assertFalse(test.validValue("iyr", "2021"));
+    }
+    @Test
+    public void testValidValueBYR(){
+        PassportValidator test = new PassportValidator();
+        assertFalse(test.validValue("byr", "1919"));
+        assertTrue(test.validValue("byr", "1920"));
+        assertTrue(test.validValue("byr", "2002"));
+        assertFalse(test.validValue("byr", "2003"));
 
+        //ecl: exactly one of amb blu brn gry grn hzl oth
+    }
 }
