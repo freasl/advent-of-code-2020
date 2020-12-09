5f5d3f8b3926608023c3aded17b93746107f11c6
Fredrik Åslund <fasl@kth.se>
2020-12-09 09:05:01 +0100

    Sync to desktop

--- day4/PassportValidator.java
+++ day4/PassportValidator.java
@@ -6,49 +6,132 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Day 4 of Advent of Code 2020!
  *
- * Passports are collected and parsed from input.txt
- *
  * Part one returns the number of valid passports according to the rules:
  *  cid is optional, all other fields must be present.
+ *
+ * @author Fredrik Åslund
+ * @version 2020-12-08
+ *
+ * @field requiredPassInfo The fields a passport must have.
+ * @field passports ArrayList where all the passports from input file are stored
  */
 public class PassportValidator {
-    private int byr;    // Birth year
-    private int iyr;    // Issue year
-    private int eyr;    // Expiration year
-    private String hgt; // Height
-    private String hcl; // Hair color
-    private String ecl; // Eye color
-    private String pid; // Passport ID
-    private String cid; // Country ID (optional!)
+    private ArrayList<String> requiredPassInfo = new ArrayList<>();
+    private ArrayList<HashMap> passports = new ArrayList();
 
-    // Obs! Läser in tomma rader också.
+    /**
+     * This method populates the list of passport fields.
+     */
+    public void populate(){
+        requiredPassInfo.add("byr"); requiredPassInfo.add("iyr"); requiredPassInfo.add("eyr");
+        requiredPassInfo.add("hgt");requiredPassInfo.add("hcl"); requiredPassInfo.add("ecl");
+        requiredPassInfo.add("pid");
+    }
+
+
+    public int partOne(String file){
+        populate();
+        collectInput(file);
+        int validCounter = 0;
+
+        for(int i = 0; i < passports.size(); i++){
+            if(passportIsValidPartOne(passports.get(i))){
+                validCounter++;
+            }
+        }
+        return validCounter;
+    }
+
+    public int partTwo(String file){
+        populate();
+        collectInput(file);
+        int validCounter = 0;
+        for(int i = 0; i < passports.size(); i++){
+            if(passportIsValidPartTwo(passports.get(i))){
+                validCounter++;
+            }
+        }
+        return validCounter;
+    }
+
+    /*
+    Valid ranges
+    byr: four digits, 1920-2002
+    iyr: four digits, 2010-2020
+    eyr: four digits, 2020-2030
+    hgt: either 150-193cm or 59-76in
+    hcl: # followed by six characters (0-9) or (a-f)
+    ecl: exactly one of amb blu brn gry grn hzl oth
+    pid: nine digit number, including leading zeroes
+    */
+    public boolean passportIsValidPartTwo(HashMap<String,String> input){
+        return false;
+    }
+    public boolean passportIsValidPartOne(HashMap<String, String> input){
+        var passport = true;
+        for(int i = 0; i< requiredPassInfo.size(); i++){
+            //String result = input.get(info);
+            if(!input.containsKey(requiredPassInfo.get(i))){
+                passport = false;
+                }
+        }
+        return passport;
+    }
+
+    // Obs! Läser in tomma rader också. Nej, fixat, men varje rad i eget element i array.
+    // Fixa: varje pass en egen hashmap, alla hashmaps i en array.
+
+    /**
+     * Method to collect passport data from a textfile.
+     * Parses the data and stores each passport as a HashMap inside the class ArrayList passports.
+     *
+     * @param file The textfile from which the passport data is collected ("./day4/input.txt")
+     */
     public void collectInput(String file){
         String strCurrentLine;
         ArrayList<String[]> array = new ArrayList<>();
+
         try {
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
+            int spaceCounter = 0;
 
             while((strCurrentLine = br.readLine()) != null){
-                array.add(strCurrentLine.split(" "));
+                if(strCurrentLine.length() != 0 ) {
+                    array.add(strCurrentLine.split(" "));
+                    }
+                if(strCurrentLine.length() == 0){
+                    spaceCounter++;
+                    HashMap<String, String> pass = new HashMap<>();
+
+                    for(int i = 0; i < array.size(); i++){
+                        for(int j = 0; j < array.get(i).length; j++){
+                            pass.put(array.get(i)[j].split(":")[0], array.get(i)[j].split(":")[1]);
+                        }
+
+                    }
+                    array.clear();
+                    passports.add(pass);
+
+                }
             }
             br.close();
             fr.close();
+            System.out.println("Number of spaces: " + spaceCounter);
         }catch (IOException e){
             e.printStackTrace();
         }
-        System.out.println(array.get(2)[0]);
-        System.out.println(array.get(2)[1]);
-        System.out.println(array.get(0)[2]);
-    }
+     }
 
     public static void main(String[] args) {
         PassportValidator test = new PassportValidator();
-        test.collectInput("./day4/input.txt");
+        System.out.println("Part one: " + test.partOne("./day4/input.txt"));
+        System.out.println("Part two: ");
     }
 }
 
--- day4/PassportValidatorTest.java
+++ day4/PassportValidatorTest.java
@@ -2,6 +2,9 @@ package day4;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.hamcrest.*;
+
+import java.util.HashMap;
 
 /**
  * Test class for PassortValidator.
@@ -15,19 +18,48 @@ public class PassportValidatorTest {
     /**
      * Set up variables to be available for each test case.
      */
-    @Before
-    public void setUp(){
 
+    public HashMap<String, String> setUp(String input){
+        HashMap<String, String> validPassport = new HashMap<>();
+        HashMap<String, String> invalidPassport = new HashMap<>();
+
+
+        validPassport.put("byr", "present"); validPassport.put("iyr", "present"); validPassport.put("eyr", "present");
+        validPassport.put("hgt", "present"); validPassport.put("hcl", "present"); validPassport.put("ecl", "present");
+        validPassport.put("pid", "present");
+
+        invalidPassport.put("byr", "present"); invalidPassport.put("iyr", "present"); invalidPassport.put("eyr", "present");
+        invalidPassport.put("hgt", "present"); invalidPassport.put("hcl", "present"); invalidPassport.put("ecl", "present");
+        if(input == "valid"){
+        return validPassport;
+        }else if(input == "invalid"){
+            return invalidPassport;
+        }
+        return null;
     }
 
     /**
      * Assert that a passport is valid.
      */
     @Test
-    public void passportIsValid(){
-        //Arrange
-        //Act
-        //Assert
+    public void assertValidPassportIsValidInPartOne(){
+        // Arrange
+        PassportValidator test = new PassportValidator();
+        HashMap<String, String> validPassport = setUp("valid");
+        test.populate();
+        // Act/Assert
+        test.passportIsValidPartOne(validPassport);
+        assertTrue(test.passportIsValidPartOne(validPassport));
+    }
+
+    @Test
+    public void assertInvalidPassportIsInvalidInPartOne(){
+        // Arrange
+        PassportValidator test = new PassportValidator();
+        HashMap<String, String> invalidPassport = setUp("invalid");
+        test.populate();
+        // Act/Assert
+        assertFalse(test.passportIsValidPartOne(invalidPassport));
     }
 
 
